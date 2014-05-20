d34da672a67092ac7456715897a757e56211035c
Ruslan Bredikhin <rus.brn@gmail.com>
2014-05-20 14:00:16 -0400

    Main functionality test.

--- test/index.test.js
+++ test/index.test.js
@@ -4,16 +4,38 @@
  * Dependencies
  */
 var should = require('should');
+var Readable = require('stream').Readable;
+var Writable = require('stream').Writable;
 var Transform = require('stream').Transform;
 var Prelog = require('../');
+var prelog = new Prelog('My prefix > ');
+var readable = Readable();
+var writable = Writable();
+
+readable._read = function () {
+  this.push('test string');
+
+  this.push(null);
+};
+
+writable._write = function(chunk, enc, next) {
+  chunk.toString().should.be.eql('My prefix > test string');
+
+  next();
+};
 
 describe('Prelog', function() {
   it('is a transform stream', function(done) {
-    var prelog = new Prelog('My prefix > ');
     prelog.should.be.an.instanceOf(Transform);
 
     done();
   });
   
-  it('should add prefix to each chunk of data');
+  it('should add prefix to each chunk of data', function(done) {
+    writable.on('finish', function() {
+      done();
+    });
+
+    readable.pipe(prelog).pipe(writable);
+  });
 });
